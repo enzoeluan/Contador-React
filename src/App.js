1d00acc80c03600615d7145134aded31480d07d6
Enzo Eluan <119829559+enzoeluan@users.noreply.github.com>
2023-09-05 10:36:17 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, {Component} from "react";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Button from './components/Button/indexbutton';
+
+import './stylecounter.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      counter: 0
+    }
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleSubtract = this.handleSubtract.bind(this)
+    this.handleReset = this.handleReset.bind(this)
+
+  }
+
+  handleAdd() {
+    this.setState(state => ({ counter: state.counter + 1 }))
+  }
+
+  handleSubtract() {
+    this.setState(state => ({ counter: state.counter - 1 }))
+  }
+
+  handleReset() {
+    this.setState({ counter: 0})
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+
+        <main>
+          <div className="display">
+            <span> {this.state.counter} </span>
+          </div>
+
+          <div className="buttons-container">
+            <Button label="-1" changeCounter={this.handleSubtract} color = "red" />
+            <Button label="Zerar" changeCounter={this.handleReset} />
+            <Button label="+1" changeCounter={this.handleAdd} color = "green" />
+          </div>
+        </main>
+
+        <Footer />
+      </div>
+    );
+  }  
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Footer extends Component {
+
+  render() {
+    return (
+        <footer>
+           <span>₢ Feito por Enzo Eluan</span> 
+        </footer>
+    );
+  }  
+}
+
+export default Footer;
